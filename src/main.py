9353562889d59b93cb01f419887920e990226b69
hiratatsu315 <tatsuhiro.hiraide@tcs.com>
2026-06-12 11:36:48 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 def add(a, b, c=0):
+  if not (isinstance(a,(int, float)) and isinstance(b,(int, float)) and isinstance(c,(int, float))):
+    return -1
+  
+  if not (0 <= a <= 10 and 0 <= b <= 10 and 0 <= c <= 10):
+    return -2
+  
   try:
     return int(a + b) + int(c)
   except:
